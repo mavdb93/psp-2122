f0e76c8694e63c2c0f52e30351fcc5f3c7c92092
mavdb93 <92082914+mavdb93@users.noreply.github.com>
2021-10-16 18:54:32 +0200

    Update Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -2,6 +2,6 @@ package prueba;
 
 public class Prueba {
     public static void main(String[] args){
-        System.out.println("Ahor alo he cmabaido desde el IDE");
+        System.out.println("Me pasere chachi");
     }
 }
