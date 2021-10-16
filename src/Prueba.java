cce519e4daf8e94b1c0c222c5b1abbcca02d0331
mavdb93 <92082914+mavdb93@users.noreply.github.com>
2021-10-16 18:53:53 +0200

    Update Prueba.java

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,5 @@
 public class Prueba {
     public static void main(String[] args){
-        System.out.println("Locambio Mundo!");
+        System.out.println("Locadawdawdawdawmbio Mundo!");
     }
 }
