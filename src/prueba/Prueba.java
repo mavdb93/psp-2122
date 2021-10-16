ff8bada357013a6c05bcb66d8bb9364b3280bf96
mavdb93 <92082914+mavdb93@users.noreply.github.com>
2021-10-16 18:58:32 +0200

    Update Prueba.java

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -2,6 +2,6 @@ package prueba;
 
 public class Prueba {
     public static void main(String[] args){
-        System.out.println("Me pasere chacdawdawdawdawhi peor mas chachi me pareses tu");
+        System.out.println("Me pasere Guaperrimo papu peor mas chachi me pareses tu");
     }
 }
