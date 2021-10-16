20b604c56c6d38bc0221e32a6603a53b3ae3de5b
mavdb93 <miguelangel.vidaldeblanca@iesvalleinclan.es>
2021-10-16 18:59:51 +0200

    Nuevo cmabioawdaw

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -2,6 +2,6 @@ package prueba;
 
 public class Prueba {
     public static void main(String[] args){
-        System.out.println("Me pasere Guaperrimo papu peor mas chachi me pareses tu");
+        System.out.println("Me pasere Gnonodds tu ere to mejón achi me pareses tu");
     }
 }
